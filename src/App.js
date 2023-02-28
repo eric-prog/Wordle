3027af17736fdbe2f77fc95373aa12338afc565a
eric-prog <eric.sprog@gmail.com>
2023-02-28 00:04:52 -0500

    Wordle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState, useEffect, useRef } from "react";
 import './App.css';
 
+// const API_URL = "https://api.frontendexpert.io/api/fe/wordle-words"
+
 function App() {
+  const [solution, setSolution] = useState("")
+  const [guesses, setGuesses] = useState(Array(6).fill(""))
+  const [gameEnd, setGameEnd] = useState(false)
+  const inputRef = useRef()
+
+  useEffect(() => {
+    // const fetchWord = async () => {
+    //   const response = await fetch(API_URL)
+    //   const words = await response.json()
+    //   const randomWord = words[Math.floor(Math.random() * words.length)] // Math.random choose random word between 0-1
+    //   setSolution(randomWord)
+    // }
+
+    // fetchWord()
+
+    setSolution("Super")
+  }, [])
+
+  const checkGameEnd = (val) => {
+    if (guesses[guesses.length-1] !== "" || gameEnd || solution === val) {
+      return true
+    }
+  }
+
+
+  const handleInput = () => {
+    const inputtedWord = inputRef.current
+    let newGuesses = guesses.slice()
+    for (let i = 0; i < guesses.length; i++) {
+      if (guesses[i] === "") {
+        newGuesses[i] = inputtedWord.value
+        break
+      }
+    }
+    setGuesses(newGuesses)
+
+    console.log(guesses)
+    if (checkGameEnd(inputtedWord.value)) {
+      setGameEnd(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Current Status: { gameEnd.toString() }</p>
+      {guesses.map((guess, i) => 
+        <Line key={i} guess={guess ?? ""} solution={solution}/>
+      )}
+      <input ref={inputRef} type="text"></input>
+      <button onClick={() => handleInput()}>Submit word</button>
     </div>
   );
+}
+
+function Line({ guess, solution }) {
+  const tiles = []
+  
+  for(let i = 0; i < 5; i++) {  
+    if (guess.length !== 0) {
+      const character = guess[i]
+      if(character === solution[i]) {
+        tiles.push(<div key={i} className="tile green">{character}</div>)
+      } else if (solution.includes(character)) {
+        tiles.push(<div key={i} className="tile gray">{character}</div>)
+      } else {
+        tiles.push(<div key={i} className="tile">{character}</div>)
+      }
+    } else {
+      tiles.push(<div key={i} className="tile"></div>)
+    }
+  }
+
+  return (
+    <div className="line">
+      { tiles }
+    </div>
+  )
 }
 
 export default App;
